b37ac4e577c31ae1b82762fbe657b4189fd51d78
Maksim Pischulenok <pischule@outlook.com>
2021-02-05 00:11:41 +0300

    add few more forms

--- src/main/java/pischule/tacocloud/DesignTacoController.java
+++ src/main/java/pischule/tacocloud/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pischule.tacocloud.Ingredient.Type;
@@ -42,6 +43,15 @@ public class DesignTacoController {
         model.addAttribute("design", new Taco());
 
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco taco) {
+        // Save the taco design
+
+        log.info("Processing design: " + taco);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
--- src/main/java/pischule/tacocloud/Taco.java
+++ src/main/java/pischule/tacocloud/Taco.java
@@ -3,9 +3,12 @@ package pischule.tacocloud;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Data
 @RequiredArgsConstructor
 public class Taco {
 
     private String name;
+    private List<String> ingredients;
 }
